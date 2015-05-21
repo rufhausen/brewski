b4cd92de1fb592718d98b6352d58d1d67c4b61ff
Gary Taylor <gary.taylor@fire-engine-red.com>
2015-05-20 18:38:27 -0600

    Custom Logging

---
+++ app/Services/ConfigureLogging.php
@@ -0,0 +1,29 @@
+<?php namespace App\Services;
+
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Foundation\Bootstrap\ConfigureLogging as BaseConfigureLogging;
+use Illuminate\Log\Writer;
+use Illuminate\Support\Facades\Log as Log;
+use Monolog\Handler\StreamHandler as StreamHandler;
+
+class ConfigureLogging extends BaseConfigureLogging
+{
+
+    /**
+     * Custom Monolog handler that for Logentries.
+     *
+     * @param  \Illuminate\Contracts\Foundation\Application  $app
+     * @param  \Illuminate\Log\Writer  $log
+     * @return void
+     */
+    protected function configureHandlers(Application $app, Writer $log)
+    {
+        $logger          = $log->getMonolog();
+        $logfile_handler = new StreamHandler(storage_path() . '/logs/laravel.log');
+        $logger->pushHandler($logfile_handler);
+        $logger->pushProcessor(new \Monolog\Processor\MemoryUsageProcessor);
+        $logger->pushProcessor(new \Monolog\Processor\MemoryPeakUsageProcessor);
+        $logger->pushProcessor(new \Monolog\Processor\WebProcessor);
+    }
+
+}
